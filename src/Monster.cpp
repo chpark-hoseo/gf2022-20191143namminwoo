fb2e08f6becc0b4337e468703035de58e5a9efdf
20191143namminwoo <20191143@vision.hoseo.edu>
2022-10-28 14:20:51 +0900

    상속 - 과제(미완)
    진행 예정

--- src/Monster.cpp
+++ src/Monster.cpp
@@ -1,1 +1,19 @@
 #include "Monster.h"
+void Monster::load(int x, int y, int width, int height, std::string textureID)
+{
+	GameObject::load(x, y, width, height, textureID);
+}
+
+void Monster::draw(SDL_Renderer* pRenderer)
+{
+	GameObject::draw(pRenderer);
+}
+
+void Monster::update()
+{
+	mob1_x -= mob1_move;
+	if (mob1_x <= 500)
+	{
+
+	}
+}

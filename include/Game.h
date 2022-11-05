7994a79b0a6805ada96730ceaa5c2b984d4e870b
20191143namminwoo <112677518+20191143namminwoo@users.noreply.github.com>
2022-11-05 09:59:00 +0900

    유일객체Game
    실습 부문 진행

--- include/Game.h
+++ include/Game.h
@@ -19,10 +19,21 @@ public:
 	void handleEvents();
 	void clean();
 
+	static Game* Instance() { //유일객체 생성
+		if (s_pInstance == 0) {
+			s_pInstance = new Game();
+			return s_pInstance;
+		}
+		return s_pInstance;
+	}
+	SDL_Renderer* getRenderer() const { return m_pRenderer; }
 
 private:
 	SDL_Window* m_pWindow;
 	SDL_Renderer* m_pRenderer;
+
+	Game() {} //생성자
+	static Game* s_pInstance; // 정적 멤버변수 
 
 	bool m_bRunning;
 
@@ -38,4 +49,4 @@ private:
 
 	//for-each 문을 위한 vector형 배열 선언
 	std::vector<int>go = {0,1};
-};
+}; typedef Game TheGame;

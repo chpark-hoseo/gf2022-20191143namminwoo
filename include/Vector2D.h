52a909bf0ecdeddc848444303df9c613188a388e
20191143namminwoo <112677518+20191143namminwoo@users.noreply.github.com>
2022-11-23 22:17:39 +0900

    키입력-실습
    오류 있음

--- include/Vector2D.h
+++ include/Vector2D.h
@@ -11,18 +11,62 @@ public:
 
 	float length() { return sqrt(m_x * m_x + m_y * m_y); }
 
-	Vector2D operator+(const Vector2D& v2) const;
-	Vector2D operator-(const Vector2D& v2) const;
-	Vector2D operator*(float scalar);
-	Vector2D operator/(float scalar);
+	Vector2D operator+(const Vector2D& v2) const
+	{
+		return Vector2D(m_x + v2.m_x, m_y + v2.m_y);
+	}
 
-	Vector2D& operator*=(float scalar);
-	Vector2D& operator/=(float scalar);
+	Vector2D operator-(const Vector2D& v2) const
+	{
+		return Vector2D(m_x - v2.m_x, m_y - v2.m_y);
+	}
+	Vector2D operator*(float scalar)
+	{
+		return Vector2D(m_x * scalar, m_y * scalar);
+	}
 
-	void normalize();
+	Vector2D operator/(float scalar)
+	{
+		return Vector2D(m_x / scalar, m_y / scalar);
+	}
 
-	friend Vector2D& operator+=(Vector2D& v1, const Vector2D& v2);
-	friend Vector2D& operator-=(Vector2D& v1, const Vector2D& v2);
+	Vector2D& operator*=(float scalar)
+	{
+		m_x *= scalar;
+		m_y *= scalar;
+		return *this;
+	}
+
+	Vector2D& operator/=(float scalar)
+	{
+		m_x /= scalar;
+		m_y /= scalar;
+		return *this;
+	}
+
+	void normalize()
+	{
+		float l = length();
+		if (l > 0) // we never want to attempt to divide by 0
+		{
+			(*this) *= 1 / l;
+		}
+	}
+
+	friend Vector2D& operator+=(Vector2D& v1, const Vector2D& v2)
+	{
+		v1.m_x += v2.m_x;
+		v1.m_y += v2.m_y;
+		return v1;
+	}
+
+	friend Vector2D& operator-=(Vector2D& v1, const Vector2D& v2)
+	{
+		v1.m_x -= v2.m_x;
+		v1.m_y -= v2.m_y;
+		return v1;
+	}
+
 
 private:
 	float m_x;
